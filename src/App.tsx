baa8210fb831f1c8ab6458596a0e7f4bd8fd9228
王强 <q13050795970@163.com>
2024-12-13 16:15:02 +0800

    feat: - logo添加 - logo动效设计

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { ConfigProvider, theme } from 'antd';
 import Container from './views/Container';
+import Logo from './components/Logo';
 
 const App: React.FC = () => {
-  return <ConfigProvider theme={{
-    algorithm: theme.darkAlgorithm
-  }}>
-    <Container />
-  </ConfigProvider>;
+  return (
+    <ConfigProvider theme={{
+      algorithm: theme.darkAlgorithm
+    }}>
+      <Logo />
+      <Container />
+    </ConfigProvider>
+  );
 }
 
 export default App;
